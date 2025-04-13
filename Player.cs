98c92d275e4143dbdd44e566c9746c70a2b346af
hmsong <114149676+songhyeonmin@users.noreply.github.com>
2025-04-13 16:49:04 +0900

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        // 3. 위치 이동
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate ()
+    {
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+        
+}
