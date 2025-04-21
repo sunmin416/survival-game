c3a378b992b5446690efbc7a6ece3d5d1b063c11
hmsong <114149676+songhyeonmin@users.noreply.github.com>
2025-04-21 22:54:36 +0900

    Update Spawner.cs

--- Spawner.cs
+++ Spawner.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
-
+    public SpawnData[] spawnData;
+    int level;
     float timer;
 
     void Awake()
@@ -13,15 +14,26 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
         
-        if (timer > 0.2f){
+        if (timer > spawnData[level].spawnTime){
             timer = 0;
             Spawn();
         }
     }
 
     void Spawn(){
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,4));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public float spawnTime;
+    public int spriteType;
+    public int health;
+    public float speed;
 }
